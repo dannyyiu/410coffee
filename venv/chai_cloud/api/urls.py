3bf3b310f978b34f89198158547e41bdcb148ca2
dannyyiu <danny.yiu@gmail.com>
2015-01-13 20:48:56 -0500

    .gitignore flush (automated)

--- venv/chai_cloud/api/urls.py
+++ venv/chai_cloud/api/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     #url(r'^api/(?P<pk>[0-9]+)/$', views.api_detail),
 
     # APIView class method
-    url(r'^api/$', views.APIList.as_view()),
-    url(r'^api/(?P<pk>[0-9]+)/$', views.APIDetail.as_view()),
+    url(r'^api$', views.APIList.as_view()),
+    url(r'^api/(?P<pk>[0-9]+)$', views.APIDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
