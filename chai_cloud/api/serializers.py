0e494337afed59b5e2a5e9229f6e49159f78f77b
dannyyiu <danny.yiu@gmail.com>
2015-01-15 03:24:50 -0500

    .gitignore refresh [AUTOMATED]

--- chai_cloud/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.forms import widgets
-from rest_framework import serializers
-from api.models import Photo
-
-class APISerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Photo
-        fields = ('path', 'title')
-
--- chai_cloud/chai_cloud/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from api.views import api_router # Import the api_router defined above
- 
-urlpatterns = patterns(
-    '', 
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include(api_router.urls)),)
